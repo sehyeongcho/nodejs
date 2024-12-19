f6b27e477dd565602f987ffc3e13cb710ccb4741
sehyeongcho <shcho4701@gmail.com>
2024-12-19 22:02:51 +0900

    test: update nextTick.js

--- test/nextTick.js
+++ test/nextTick.js
@@ -7,10 +7,10 @@ const callback = () => {
 };
 
 setImmediate(() => console.log('setImmediate 비동기 함수의 콜백 함수입니다.'));
-// Check 단계에서 처리됩니다. 하지만 nextTick의 재귀 호출로 인해 실행되지 못합니다.
+// Check 페이즈에서 처리됩니다. 하지만 nextTick의 재귀 호출로 인해 실행되지 못합니다.
 
 setTimeout(() => console.log('setTimeout 비동기 함수의 콜백 함수입니다.'), 100);
-// Timers 단계에서 처리됩니다. 하지만 nextTick의 재귀 호출로 인해 실행되지 못합니다.
+// Timers 페이즈에서 처리됩니다. 하지만 nextTick의 재귀 호출로 인해 실행되지 못합니다.
 
 process.nextTick(callback);
 // nextTick은 이벤트 루프의 시작 시, 그리고 각 페이즈 사이에서 처리되므로 다른 비동기 함수들보다 먼저 실행됩니다.
