74f9988b68193b5d4a2594c9b31024d9395bd6dc
sehyeongcho <shcho4701@gmail.com>
2024-12-20 11:19:35 +0900

    test: create eventEmitter.js

---
+++ test/eventEmitter.js
@@ -0,0 +1,18 @@
+const EventEmitter = require("events");
+
+const subject = new EventEmitter();
+// subject (observer design pattern의 관찰 대상 역할할)
+
+subject.on("updatePost", () => {
+  console.log("This post is so awesome!");
+});
+// observer 1: updatePost 이벤트 발생 시 실행될 콜백 등록
+// on 메서드는 특정 이벤트가 발생할 때 실행될 리스너를 추가합니다.
+
+subject.on("updatePost", () => {
+  console.log("I like this post!");
+});
+// observer 2: 동일 이벤트에 추가적인 리스너 등록
+
+subject.emit("updatePost");
+// emit 메서드는 updatePost 이벤트를 트리거하여, 등록된 모든 리스너를 실행합니다.
