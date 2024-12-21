1bede0ffb8d57fb75da08257e9e7341ed978a99b
sehyeongcho <shcho4701@gmail.com>
2024-12-21 18:02:54 +0900

    test: update httpServer.js

--- test/httpServer.js
+++ test/httpServer.js
@@ -7,7 +7,10 @@ const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.end("<h1>Hello, World!</h1>");
 });
+// 모듈을 사용하여 HTTP 서버를 만듭니다.
 
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
+// 서버는 지정된 포트 3000에서 수신 대기하도록 설정됩니다.
+// 서버가 준비되면 콜백 함수가 호출됩니다.
