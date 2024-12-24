10ea1f16ebc672a2d6d5c60eb1339446046a54f8
sehyeongcho <shcho4701@gmail.com>
2024-12-24 17:14:08 +0900

    Revert "test: create server_routing.js"
    This reverts commit 3053394b72acd49be362ed39dc85d299188ca439.

--- test/web_server/server_routing.js
+++ test/web_server/server_routing.js
@@ -2,38 +2,27 @@ const http = require("http");
 
 const port = 3000;
 
-const dataObject = {
-  a: "a",
-  b: "b",
-};
-
 const server = http.createServer((req, res) => {
   // 요청 URL에 따라 분기 처리합니다.
-  if (req.method === "POST" && req.url === "/home") {
-    req.on("data", (data) => {
-      console.log("data", data);
-      const stringifiedData = data.toString();
-      console.log("stringifiedData", stringifiedData);
-      Object.assign(dataObject, JSON.parse(stringifiedData));
-    });
+  if (req.url === "/home") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({
+      a: "a",
+      b: "b",
+    }));
+  } else if (req.url === "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<h1>About Page</h1>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
   } else {
-    if (req.url === "/home") {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify(dataObject));
-    } else if (req.url === "/about") {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html");
-      res.write("<html>");
-      res.write("<body>");
-      res.write("<h1>About Page</h1>");
-      res.write("</body>");
-      res.write("</html>");
-      res.end();
-    } else {
-      res.statusCode = 404;
-      res.end();
-    }
+    res.statusCode = 404;
+    res.end();
   }
 });
 
